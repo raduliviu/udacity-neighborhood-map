34067acda59adaa58885720bbc664b879e97fede
unknown <baltag.radu@gmail.com>
2018-11-15 14:39:27 +0100

    feat: can toggle markers from side menu with enter

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,9 +1,16 @@
 import React, {Component} from "react";
 
 export default class ListItem extends Component {
+    handle = (e) => {
+        if(e.keyCode === 13){
+            e.preventDefault();
+            this.props.handleListItemClick(this.props)
+        }
+    }
+    
     render() {
         return (
-            <li tabIndex="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)}>
+            <li tabIndex="0" className="listItem" onClick={() => this.props.handleListItemClick(this.props)} onKeyDown={this.handle}>
                 <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} alt={this.props.categories[0].name}/>
                 {this.props.name}
             </li>
